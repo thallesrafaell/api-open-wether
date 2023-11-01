1854743dfe9f64db6b19e9d1f848d8becf52787f
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-01 20:41:18 -0300

    CRIANDO CONFIGURAÇÕES DO PROJETO

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+
+function compilaSass() {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/styles/'))
+}
+
+function comprimeJs() {
+    return gulp.src('./src/scripts/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts/main.js'))
+}
+
+function comprimeImg() {
+    return gulp.src('./src/images/**.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/imagess/'))
+}
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(compilaSass))
+    gulp.watch('./src/scripts/main.js',{ignoreInitial:false}, gulp.parallel(comprimeJs))
+    gulp.watch('./src/images/**.*', {ignoreInitial: false}, gulp.parallel(comprimeImg))
+}
+
+exports.build = gulp.parallel(compilaSass, comprimeImg, comprimeJs)
