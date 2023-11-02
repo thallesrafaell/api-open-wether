4e998162be9837dfa73a93fe82fac400866bd9bf
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-02 00:59:22 -0300

    ADICIONANDO CSS E CONSUMINDO API

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,45 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const uglify = require('gulp-uglify')
-const imagemin = require('gulp-imagemin')
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
 
 function compilaSass() {
-    return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/styles/'))
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("./dist/styles/"));
 }
 
 function comprimeJs() {
-    return gulp.src('./src/scripts/main.js')
+  return gulp
+    .src("./src/scripts/main.js")
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/scripts/main.js'))
+    .pipe(gulp.dest("./dist/scripts/"));
 }
 
 function comprimeImg() {
-    return gulp.src('./src/images/**.*')
+  return gulp
+    .src("./src/images/**.*")
     .pipe(imagemin())
-    .pipe(gulp.dest('./dist/imagess/'))
+    .pipe(gulp.dest("./dist/imagess/"));
 }
 
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(compilaSass))
-    gulp.watch('./src/scripts/main.js',{ignoreInitial:false}, gulp.parallel(comprimeJs))
-    gulp.watch('./src/images/**.*', {ignoreInitial: false}, gulp.parallel(comprimeImg))
-}
+exports.watch = function () {
+  gulp.watch(
+    "./src/styles/**.scss",
+    { ignoreInitial: false },
+    gulp.parallel(compilaSass)
+  );
+  gulp.watch(
+    "./src/scripts/main.js",
+    { ignoreInitial: false },
+    gulp.parallel(comprimeJs)
+  );
+  gulp.watch(
+    "./src/images/**.*",
+    { ignoreInitial: false },
+    gulp.parallel(comprimeImg)
+  );
+};
 
-exports.build = gulp.parallel(compilaSass, comprimeImg, comprimeJs)
+exports.build = gulp.parallel(compilaSass, comprimeImg, comprimeJs);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,59 @@
-console.log("Olá, mundo")
+
+//evento de carregamento
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoBuscar = document.getElementById("btn-buscar");
+  //evento de click
+ botaoBuscar.addEventListener('click', function(e){
+    const hide = document.getElementById('resultado')
+
+    e.preventDefault()
+    chamadaApi()
+    
+    hide.classList.remove('conteudo__resultado--hidde')
+  })
+});
+
+
+//funcao fech
+function chamadaApi() {
+    const apiKey = "bc6e0025a1b550b6e2fa7fd5193edd7d";
+    const cidade = document.getElementById("cidade").value;
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`;
+    
+    fetch(endpoint)
+    .then(function (resposta) {
+        return resposta.json()
+    })
+    .then(function(json){
+        //seletores da api 
+        console.log(json.wind.speed)
+        const apiCidade = json.name
+        const apiflag = json.sys.country
+        const apiTemp = Math.floor(json.main.temp)
+        const apiTempo = json.weather[0].description
+        const apiTempoIcon = json.weather[0].icon
+        const apiHumidade = json.main.humidity
+        const apiVelocidade = Math.floor(json.wind.speed)
+
+        //seletores do html
+        const respostaCidade = document.getElementById('reposta-cidade')
+        const flag = document.getElementById('flag')
+        const graus = document.getElementById('graus')
+        const tempo = document.getElementById('tempo')
+        const tempoIcon = document.getElementById('tempo-icon')
+        const humidade = document.getElementById('humidade')
+        const velocidade = document.getElementById('velocidade')
+        
+
+
+        //manipulação do DOM
+        respostaCidade.textContent =  apiCidade 
+        flag.setAttribute('src', `https://flagsapi.com/${apiflag}/flat/64.png`)
+        graus.innerHTML = `${apiTemp}°C`
+        tempo.textContent = apiTempo
+        tempoIcon.setAttribute('src', `https://openweathermap.org/img/wn/${apiTempoIcon}@2x.png`)
+        humidade.textContent = `${apiHumidade}%`
+        velocidade.textContent =`${apiVelocidade} Km/h`
+        
+    })
+}
